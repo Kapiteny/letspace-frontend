ef98e1b2098078931a797b72fe01650cdeedf52b
Kapiteny <tombontsoaharisanda@gmail.com>
2026-04-28 16:46:59 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-blue-500 gap-10'>
+      <h1 className='text-4xl font-sans text-white font-bold'>
+        hello tailwindcss v3
+      </h1>
+      <h1 className='text-4xl font-sans text-white font-bold'>
+        Test Direct CSS
+      </h1>
+    </div>
+  )
+}
+
+export default App
